4d2d526357c3b7dd9b79b30c2beaf56e9ef29b68
pimpmyname3 <111854281+pimpmyname3@users.noreply.github.com>
2025-06-08 16:50:26 +0200

    Update comment-ci-results.py

--- .github/workflows/scripts/comment-ci-results.py
+++ .github/workflows/scripts/comment-ci-results.py
@@ -229,7 +229,7 @@ def main():
         for trading_mode in reports_info[exchange]:
           for name in sorted(reports_info[exchange][trading_mode]):
             value = reports_data[exchange][trading_mode][name]["results"].get(timerange, {}).get(key, "n/a")
-            reports_data[exchange]["timeranges"][timerange][key][trading_mode][name] = value
+            reports_data[exchange]["timeranges"][timerange][key][name] = value
 
   pprint.pprint(reports_data)
 
